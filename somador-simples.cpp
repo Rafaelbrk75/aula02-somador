9fa3b7318d0a4c39054fc489e0bc9f953f3475ec
Rafael Rodrigues <88215458+Rafaelbrk75@users.noreply.github.com>
2024-08-14 11:08:50 -0300

    Add files via upload

---
+++ somador-simples.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int a,b,s;
+	
+	printf("Digite o vaalor de a: ");
+	scanf("%d", &a);
+	printf("Digite o valor de b: ");
+	scanf("%d", &b);
+	s = a+b
+	printf("A soma de %d + %d = %d", a,b,s);
+	
+	return 0;
+}
